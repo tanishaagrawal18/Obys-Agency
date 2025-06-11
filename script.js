dde13af0dc49b56daf705890cd556d386ca49f3d
tanishaagrawal18 <agrawaltanisha222@gmail.com>
2025-06-12 01:18:06 +0530

    successfully made page1 and page2

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var tl = gsap.timeline();
+function loadinganimation(){
+    var tl = gsap.timeline();
 tl.from(".line h1", {
   y: 150,
   stagger: 0.2,
@@ -24,12 +25,12 @@ tl.to(".line h2",{
 tl.to("#loader", {
   opacity: 0,
   duration: 0.4,
-  delay: 3.4,
+  delay: 0,
 });
 tl.from("#page1",{
     y: 1600,
     duration: 0.5,
-    delay:0.2,
+    delay:0.1,
     opacity: 0,
     ease: "power2"
 })
@@ -37,3 +38,31 @@ tl.to("#loader",{
     display: "none",
     
 })
+tl.from("nav",{
+    opacity: 0,
+    
+})
+
+tl.from("#center1 h1,#center2 h1,#center3 h2,#center4 h1",{
+  y: 110,
+  stagger: 0.2,
+})
+}
+loadinganimation();
+function cursorAnimation(){
+  document.addEventListener("mousemove",function(dets){
+  gsap.to("#crsr", {
+     left: dets.x,
+      top: dets.y,
+    })
+})
+Shery.makeMagnet("#nav-part2 h3" , {
+  
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+   y: 0.7,
+   x: 0.7,
+  range: 180
+});
+}
+cursorAnimation();
